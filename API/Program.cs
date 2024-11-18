c42a9f7da6db78ec03485c40df6683e31fcc6559
nasser-git <yazdanpournasser@gmail.com>
2024-11-17 16:36:30 -0800

    End of Section 4

--- API/Program.cs
+++ API/Program.cs
@@ -1,19 +1,11 @@
-using API.Data;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore.Builder;
+using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddCors();
-
-
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration); 
+   
 // Add CORS policy
 builder.Services.AddCors(options =>
 {
@@ -29,12 +21,14 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
 // app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("htpp://localhost:4200", "htpps://localhost:4200"));
 
 
 // Apply CORS policy
 app.UseCors("AllowSpecificOrigins");
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
